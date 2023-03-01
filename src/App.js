4aa5e4305e253e334852f87d1decaa583bfa1425
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-01 10:51:23 +0300

    sections created

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <nav>
         {/* a logo image */}
           <a  className="logo">
-          
             <img src={logo} alt="Logo" />
           </a>
           <ul>
@@ -20,68 +19,25 @@ function App() {
         </nav>
       </header>
 
-      {/* Services Section */}
+      {/* About Section */}
       <section id="About" className="services-section">
         <div className="services-container">
           <h2>About</h2>
-          <div className="services-grid">
-            <div className="service-card">
-              <h3>Web Design</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div className="service-card">
-              <h3>Graphic Design</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div className="service-card">
-              <h3>SEO</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
+       
         </div>
       </section>
 
-      {/* Team Section */}
+      {/* How it works Section */}
       <section id="How it works" className="team-section">
         <div className="team-container">
           <h2>How it works</h2>
-          <div className="team-grid">
-            <div className="team-card">
-              <img src="https://via.placeholder.com/150" alt="Team Member" />
-              <h3>John Doe</h3>
-              <p>Co-Founder</p>
-            </div>
-            <div className="team-card">
-              <img src="https://via.placeholder.com/150" alt="Team Member" />
-              <h3>Jane Doe</h3>
-              <p>Designer</p>
-            </div>
-            <div className="team-card">
-              <img src="https://via.placeholder.com/150" alt="Team Member" />
-              <h3>Bob Smith</h3>
-              <p>Developer</p>
-            </div>
-          </div>
-        </div>
+         </div>
       </section>
+
     {/* contact Section */}
     <section id="Help" className="contact-section">
         <div className="services-container">
           <h2>Help</h2>
-          <div className="services-grid">
-            <div className="service-card">
-              <h3>Web Design</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div className="service-card">
-              <h3>Graphic Design</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div className="service-card">
-              <h3>SEO</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-          </div>
         </div>
       </section>
 
