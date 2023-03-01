b6acbe97c552eb7607f69ab592c3c4552ec01fc3
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-01 12:28:16 +0300

    Faqs added and page updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import logo from './Images/logo.png';
 import Africa from './Images/AFRICA.png';
+import Partners from './Images/Partners.png'
+
+
 function App() {
   return (
     <div className="App">
@@ -35,16 +38,72 @@ function App() {
 
       {/* How it works Section */}
       <section id="works" className="works-section">
-        <div className="works-container">
+        <div className="works">
           <h2>How to Send Money With Kayshka</h2>
+          <div className='works-container'>
+            <div className='works-box'>
+              <h3 className='circle'>1</h3>
+              <h4>Register</h4>
+              <p>This is through your phone number and other details and choose a strong pin.</p>
+              </div>
+            <div className='works-box'>
+              <h3 className='circle'>2</h3>
+              <h4>Access Services</h4>
+              <p>This is by Entering your Phone number and Pin </p>
+              </div>
+            <div className='works-box'>
+              <h3 className='circle'>3</h3>
+              <h4>Choose Services</h4>
+              <p>Select the services you require, and start using them.</p>
+              </div>
+          </div>
+          {/* Get services button */}
+          <button className='service-btn'>Get services</button>
          </div>
       </section>
 
-    {/* contact Section */}
+    {/* Parties invovled Section */}
+    <section id="Parties" className="parties-section">
+        <div className="parties-container">
+          <h2>Parties Involved</h2>
+          
+          {/*partners image*/}
+          <img src={Partners} alt="Partners"/>
+          </div>
+          </section>
+
+    {/* help Section */}
     <section id="Help" className="help-section">
         <div className="help-container">
           <h2>Everything you need to know about sending money With KAYSHKA</h2>
-        </div>
+          {/* Faqs questions */}
+          <details>
+             <summary>How do I register with Kayshka?</summary>
+              <p>You can register with Kayshka by providing your 
+              phone number and other details and choosing a strong pin.
+              </p>
+           </details>
+
+            <details>
+              <summary>How do I access my account?</summary>
+              <p>You can access your account by entering your phone number and pin.</p>
+            </details>
+
+            <details>
+              <summary>How do I send money?</summary>
+              <p>You can send money by selecting the send money option and entering the amount you want to send and the phone number of the person you want to send the money to.</p>
+            </details>
+
+            <details>
+              <summary>How do I receive money?</summary>
+              <p>You can receive money by selecting the receive money option and entering the amount you want to receive and the phone number of the person you want to receive the money from.</p>
+            </details>
+            
+
+
+
+
+       </div>
       </section>
 
       {/* Footer */}
