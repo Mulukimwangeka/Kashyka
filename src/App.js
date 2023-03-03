e8e2a6d194738a9a4160d3bdc8feb83798d1efff
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-03 12:43:52 +0300

    app updated

--- src/App.js
+++ src/App.js
@@ -82,8 +82,7 @@ function App() {
           <h2 className='about-heading'>WHAT IS <span className='Green-highlight'>KASH</span>YKA?</h2>
           <p className='about-content'>A Secure and speedy money transfer platform </p>
           <p className='about-content'>for africa that prioritizes financial </p>
-          <p className='about-content'> inclusion and accessibility </p>
-          <p className='about-content'>for all.</p>
+          <p className='about-content'> inclusion and accessibility for all.</p>
           {/* IMAGE */}
           {/* <img className='about-image' src={Africa} alt="Africa"/> */}
         </div>
