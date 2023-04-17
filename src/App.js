bfffc794ef203ffeda89fe7cd4f49866be766831
mulukimwangeka <mwangekaruth@gmail.com>
2023-04-17 14:41:08 +0300

    logo updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import logo from './Images/logo1.png';
+import logo from './Images/FastaPayLogo.png';
 import Partners from './Images/Partners.png'
 import about from './Images/newabout.png'
 import { faFacebookF, faTwitter, faInstagram, faTelegram, faWhatsapp, faLinkedin } from '@fortawesome/free-brands-svg-icons';
